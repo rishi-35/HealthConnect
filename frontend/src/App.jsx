5fc5bf8841c24929b769b87edbffffa8a5e6d3b7
rishi <rishikonga3@gmail.com>
2025-05-19 21:31:54 +0530

    fixing deployement error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import './App.css';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import AuthPage from './pages/AuthPage';
 import { useAuthStore } from './store/authStore';
 import { Toaster } from 'react-hot-toast';
